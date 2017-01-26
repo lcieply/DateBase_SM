e2e6030a5309bc2b391d3b8c7b51b114755ee4fd
Kamil Jureczka <kamil.jureczka@hotmail.com>
2017-01-26 22:11:21 +0100

    SMFactory class added

---
+++ Tmp_Client/include/SMFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by student on 26.01.17.
+//
+
+#ifndef TMP_CLIENT_SMFACTORY_H
+#define TMP_CLIENT_SMFACTORY_H
+
+
+class SMFactory {
+
+};
+
+
+#endif //TMP_CLIENT_SMFACTORY_H
---
+++ Tmp_Client/src/SMFactory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by student on 26.01.17.
+//
+
+#include "../include/SMFactory.h"
