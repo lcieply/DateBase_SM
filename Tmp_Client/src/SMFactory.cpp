c8e59bb34dc9983b0f4495c4d421baf6be772ba8
Kamil Jureczka <kamil.jureczka@hotmail.com>
2017-01-29 17:43:49 +0100

    SMSystem temporary client ofiszjaln werszyn

--- Tmp_Client/src/SMFactory.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by student on 26.01.17.
-//
-
-#include "../include/SMFactory.h"
--- Tmp_Client/src/main.cpp
+++ Tmp_Client/src/main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include "../lib/SMSystem.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    SMSystem system;
+
+    SMType<int> liczba(6);
+    SMType<char> znak('k');
+
+    std::cout << "Wartosc liczby przed wstawieniem: " << liczba.val << std::endl;
+    std::cout << "Wartosc znaku przed wstawieniem: " << znak.val << std::endl;
+
+    if(system.contains("k1")) {
+        std::cout << "Usuwam obiekt pod kluczem k1!\n";
+        try {
+            system.remove("k1");
+        } catch (SMSystemException e){
+           std::cout << e.what() << std::endl;
+        }
+    }
+
+    if(system.contains("k2")) {
+        std::cout << "Usuwam obiekt pod kluczem k2!\n";
+        try {
+            system.remove("k2");
+        } catch (SMSystemException e){
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+    //=================================================
+
+    std::cout << "Wstawiam k1!\n";
+    system.insert("k1", &liczba);
+
+    std::cout << "Wstawiam k2!\n";
+    system.insert("k2", &znak);
+
+    //=================================================
+
+    SMType<int> liczba2;
+    system.fetch("k1", liczba2);
+
+    SMType<char> znak2;
+    system.fetch("k2", znak2);
+
+    std::cout << "Wartosc liczby po wyjeciu: " << liczba2.val << std::endl;
+    std::cout << "Wartosc znaku po wyjeciu: " << znak2.val << std::endl;
     return 0;
 }
